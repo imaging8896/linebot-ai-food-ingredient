addecab7273f4413d0284ebac1a29018ba668d04
Ted Chen <imaging8896@gmail.com>
2025-09-29 15:26:36 +0800

    Add mongo db in app context

--- food/app_config.py
+++ food/app_config.py
@@ -15,6 +15,11 @@ PORT = int(os.environ['PORT']) if os.getenv('PORT', None) else None
 LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
 
+MONGO_DB_CLOUD_HOST = os.environ['MONGO_DB_CLOUD_HOST']
+MONGO_DB_DATABASE = os.environ['MONGO_DB_DATABASE']
+MONGO_DB_USER = os.environ['MONGO_DB_USER']
+MONGO_DB_PASSWORD = os.environ['MONGO_DB_PASSWORD']
+
 if ENV is not None:
     LOG_CONFIG = "logging.yml"
     DEBUG = False
--- food/app_context.py
+++ food/app_context.py
@@ -1,4 +1,16 @@
 import logging
 
+import app_config
+
+from food.db.mongo_db import MongoDB
+
+mongo_db = MongoDB(
+    MongoDB.Parameter(
+        host=app_config.MONGO_DB_CLOUD_HOST,
+        user=app_config.MONGO_DB_USER,
+        password=app_config.MONGO_DB_PASSWORD,
+        database=app_config.MONGO_DB_DATABASE,
+    )
+)
 
 app_logger = logging.getLogger("app")
