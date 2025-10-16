7750be2a0fe3d2d21c9ef99ac219828fd8076765
Ted Chen <imaging8896@gmail.com>
2025-10-16 17:44:01 +0800

    refactor: [line][message] add pagination for text message

--- food/line/message/text.py
+++ food/line/message/text.py
@@ -3,9 +3,26 @@ from linebot.v3.messaging import TextMessage
 from .component.quick_reply import quick_reply_general
 
 
-def flex_message_text(text: str, quote_token: str | None = None) -> TextMessage:
-    return TextMessage(
-        text=text,
-        quoteToken=quote_token,
-        quickReply=quick_reply_general(),
-    )
+def flex_message_texts(text: str, quote_token: str | None = None) -> list[TextMessage]:
+    max_message_length = 1000
+    max_message_body_count = 5
+    messages = []
+    for i in range(0, len(text), max_message_length):
+        if i == max_message_body_count - 1:
+            messages.append(
+                TextMessage(
+                    text=text[i:i+max_message_length] + "\n過多無法顯示...", 
+                    quoteToken=quote_token, 
+                    quickReply=quick_reply_general(),
+                )
+            )
+        else:
+            messages.append(
+                TextMessage(
+                    text=text[i:i+max_message_length],
+                    quoteToken=quote_token,
+                    quickReply=quick_reply_general(),
+                )
+            )
+
+    return messages
